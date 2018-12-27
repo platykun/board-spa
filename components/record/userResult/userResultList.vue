24fef1e717d422c6bb597d85e70b18d894cc2c47
platykun <nagonago561@yahoo.co.jp>
2018-12-27 16:39:06 +0900

    スペルミス見直し

--- components/record/userResult/userResultList.vue
+++ components/record/userResult/userResultList.vue
@@ -64,7 +64,7 @@
 
 <script>
 import UserResultModal from '~/components/record/userResult/userResultModal';
-import UpdateUserResultModal from '~/components/record/userResult/UpdateUserResultModal';
+import UpdateUserResultModal from '~/components/record/userResult/updateUserResultModal';
 
 export default {
   components: {
